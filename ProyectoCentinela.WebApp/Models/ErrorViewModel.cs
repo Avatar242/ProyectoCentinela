bd107768dd9dcc269b1fb21566153cdfa4960f92
Avatar242 <165806102+Avatar242@users.noreply.github.com>
2025-07-02 19:31:23 +0000

    Mi enesimo primer commit

---
+++ ProyectoCentinela.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoCentinela.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
